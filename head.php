fd2c428bf9dd20e6905ba55eb1bd973250884517
NGN-Limjihoon <lonelynight1026@gmail.com>
2023-03-28 02:00:29 +0900

    fix : highjump 신기록 저장 로직 완성

--- head.php
+++ head.php
@@ -5,8 +5,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./assets/css/xeicon.min.css">
-    <link rel="stylesheet" href="./assets/css/swiper.min.css">
-    <link rel="stylesheet" href="./assets/css/reset.css?v=1">
-    <link rel="stylesheet" href="./assets/css/style.css?v=46">
+    <link rel="stylesheet" href="/assets/css/xeicon.min.css">
+    <link rel="stylesheet" href="/assets/css/swiper.min.css">
+    <link rel="stylesheet" href="/assets/css/reset.css?v=1">
+    <link rel="stylesheet" href="/assets/css/style.css?v=46">
     <title>u20 관리자 페이지</title>

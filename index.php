3fe68737976d04c8ab2e10faf37887cb97b19b77
unknown <lonelynight1026@gmail.com>
2023-03-27 22:08:25 +0900

    github err test : index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ session_start();
 
 ini_set('error_reporting', E_ALL);
 ini_set('display_errors', true);
-require_once "backheader.php";
+require_once __DIR__ . "backheader.php";
 
 if (!isset($_SESSION['Id'])) {
     echo "<script>location.replace('login.php');</script>";

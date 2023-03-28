a5dd3292159d6a5efaff463c1a5ecb0e31714a77
gwonsan <jmt30269@gmail.com>
2023-03-29 01:48:45 +0900

    fix:기록이 0일때도 등수 저장

--- action/record/field_normal_result_insert.php
+++ action/record/field_normal_result_insert.php
@@ -151,7 +151,7 @@ for ($i = 0; $i < count($athlete_name); $i++) {
             if($result[$i]>=9 && $ruf == 'X'){
                 $cnt++;
             }
-                if ((float)$highrecord < (float)$ruf && $ruf != 'X') {
+                if ((float)$highrecord <= (float)$ruf && $ruf != 'X') {
                 $highrecord = $ruf;
                 $hightrial = $k;
             }
